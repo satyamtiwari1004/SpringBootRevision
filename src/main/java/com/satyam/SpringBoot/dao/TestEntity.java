fa5162b1ad263eeb1e457703dea9cece4701bc6b
satyamtiwari1004 <satyamtiwari1004@gmail.com>
2025-05-05 01:16:22 +0530

    Upgraded SpringBoot Version & Added Swagger UI configuration

---
+++ src/main/java/com/satyam/SpringBoot/dao/TestEntity.java
@@ -0,0 +1,28 @@
+package com.satyam.SpringBoot.dao;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class TestEntity {
+    @Id
+    private Long id;
+    private String name;
+
+    // Getters and setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
--- src/main/java/com/satyam/SpringBoot/repo/TestRepo.java
+++ src/main/java/com/satyam/SpringBoot/repo/TestRepo.java
@@ -1,6 +1,10 @@
 package com.satyam.SpringBoot.repo;
 
+import com.satyam.SpringBoot.dao.TestEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 @Repository
-public interface TestRepo extends JPARepository<String,String>{
+public interface TestRepo extends JpaRepository<TestEntity, Long> {
 
 }
